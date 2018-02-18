db564905204168128de972fa89cef82f6550ceea
BettinaP <bettinaprophete@gmail.com>
2018-02-17 20:36:49 -0500

    created UI via storyboard

--- WeatherToday/ViewController.swift
+++ WeatherToday/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var locationNameLabel: UILabel!
+    @IBOutlet weak var weatherSummaryLabel: UILabel!
+    @IBOutlet weak var locationImageView: UIImageView!
+    var city = String()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+    }
+    
 }
-
